134c30908d865a89c45d6c55ef0bf9924ddba792
Abubakir Kais <bakir_janabi@yahoo.com>
2022-04-17 16:21:57 +0400

    Initial Commit

---
+++ instagram-clone-ios/instagram_clone_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_clone_iosApp.swift
+//  instagram-clone-ios
+//
+//  Created by Abubakir  Kais on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct instagram_clone_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
