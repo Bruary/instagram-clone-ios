b03fe4c69ad6bece09ce0b5b9d1a9b9f62457c83
Abubakir Kais <bakir_janabi@yahoo.com>
2022-04-27 03:38:45 +0400

    Making post model adjustment

--- Models/PostModel.swift
+++ Models/PostModel.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-struct Post {
+struct Post: Identifiable {
+    var id = UUID()
     var PostURL: String
     var Caption: String
     var LikesCount: Int
-    var UserID: Int
     var CreatedAt: String
 }
+
+var posts : [Post] = [
+    Post(PostURL: "user1_image1", Caption: "See my new game hahahahaah.", LikesCount: 97, CreatedAt: "12/05/2021"),
+    Post(PostURL: "user1_image2", Caption: "New journey in Expo dubai 123.", LikesCount: 50, CreatedAt: "09/02/2021"),
+    Post(PostURL: "user1_image3", Caption: "UAE building in UAE.", LikesCount: 128, CreatedAt: "20/02/2021"),
+]
 
 
 
