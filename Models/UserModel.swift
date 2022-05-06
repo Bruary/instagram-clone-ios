bcfd6990a523278497a709a237a0a48c0dd5fa0a
Abubakir Kais <bakir_janabi@yahoo.com>
2022-05-07 01:49:11 +0400

    Adding users as User array

--- Models/UserModel.swift
+++ Models/UserModel.swift
@@ -7,11 +7,19 @@
 
 import Foundation
 
-struct User {
+struct User: Identifiable {
+    var id = UUID()
     var Name: String
     var Email: String
     var ProfileImageUrl: String
     var ProfilePublicID: String
 }
+
+var users: [User] = [
+    User(Name: "Jackie", Email: "jackie@gmail.com", ProfileImageUrl: "user1_profile", ProfilePublicID: "#abc"),
+    User(Name: "Richard", Email: "richard@gmail.com", ProfileImageUrl: "user2_profile", ProfilePublicID: "#cdb"),
+    User(Name: "Frank", Email: "frank@gmail.com", ProfileImageUrl: "user3_profile", ProfilePublicID: "#cdf")
+    
+]
 
 var firstUser: User = User(Name: "Jackie", Email: "jackie@gmail.com", ProfileImageUrl: "user1_profile", ProfilePublicID: "#abc")
